c3bf71ebfc6027d687fa5ac9279b7f8378790c00
allaponomarenko <alla.deyneka03@gmail.com>
2024-03-09 12:52:58 +0100

    corregido-1

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
     const borderWidth = parseFloat(border);
-    const totalWidth = contentWidth + 2 * (contentWidth + borderWidth);
+    const totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
     return totalWidth; 
 }
 
