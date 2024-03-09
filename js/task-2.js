3a5c5484f95faa1231fde07ca258f4ae2009624d
allaponomarenko <alla.deyneka03@gmail.com>
2024-03-09 12:19:00 +0100

    corregido-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function getShippingMessage(country, price, deliveryFee) {
-    const totalaPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalaPrice} credits`
+    const totalPrice = price + deliveryFee;
+    return `Shipping to ${country} will cost ${totalPrice} credits`
 }
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
